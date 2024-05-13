e3a4615748a021e92d1e73fe9f93db3ebd2dbe71
dav.melk <dav.melk.2002@gmail.com>
2024-05-13 09:18:03 +0400

    Delete cmake files

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/davit/Documents/tdd_chess
-# Build directory: /home/davit/Documents/tdd_chess/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[Name]=] "MyTests" "Command" "MyTests")
-set_tests_properties([=[Name]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/davit/Documents/tdd_chess/CMakeLists.txt;27;add_test;/home/davit/Documents/tdd_chess/CMakeLists.txt;0;")
