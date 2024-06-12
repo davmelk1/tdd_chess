7033794e40d13b8e6b612095b590bdda67c94bb6
davit <dav.melk.2002@gmail.com>
2024-06-12 14:04:40 +0400

    Add moving bishop to available cells

--- include/bishop.h
+++ include/bishop.h
@@ -9,5 +9,5 @@ public:
 
     std::forward_list<Cell*>
     get_all_available_moves(std::array<std::array<Cell, constants::BOARD_SIZE>, constants::BOARD_SIZE>& board,
-                            const Cell* selected_cell) const override {return {};};
+                            const Cell* selected_cell) const override;
 };
--- src/bishop.cpp
+++ src/bishop.cpp
@@ -3,3 +3,33 @@
 Bishop::Bishop(ColorEnum color) : Figure(color) {
     sprite.setTextureRect(get_figure_rect_from_name_and_color("bishop"));
 }
+
+std::forward_list<Cell *>
+Bishop::get_all_available_moves(std::array<std::array<Cell, constants::BOARD_SIZE>, constants::BOARD_SIZE> &board,
+                                const Cell *selected_cell) const {
+    unsigned int i  = (selected_cell - &board[0][0]) / board.size();
+    unsigned int j = selected_cell - &board[i][0];
+    std::forward_list<Cell*> available_moves;
+
+    int jj = j, ii = i;
+    while (++jj < board[0].size() && --ii >=0)
+        if (!set_available_if_so_and_return_status(board[ii][jj], available_moves))
+            break;
+
+    jj = j, ii = i;
+    while (--jj >= 0 && --ii >= 0)
+        if (!set_available_if_so_and_return_status(board[ii][jj], available_moves))
+            break;
+
+    jj = j, ii = i;
+    while (++jj < board[0].size() && ++ii < board.size())
+        if (!set_available_if_so_and_return_status(board[ii][jj], available_moves))
+            break;
+
+    jj = j, ii = i;
+    while (--jj >= 0 && ++ii < board.size())
+        if (!set_available_if_so_and_return_status(board[ii][jj], available_moves))
+            break;
+
+    return available_moves;
+}
