8fc90f834fd15793429185dcc54e73cbfce83db7
davit <dav.melk.2002@gmail.com>
2024-06-14 09:30:47 +0400

    Add tests for Pawn

--- tests_include/pawn_test.h
+++ tests_include/pawn_test.h
@@ -34,7 +34,7 @@ TEST_F(DerivedFromBoard, TestAvailableMovesForBlackPawn) {
     EXPECT_EQ(distance(moves.begin(), moves.end()), 2);
 }
 
-TEST_F(DerivedFromBoard, TestAvailableMovesForBlackPawnOnSecondStep) {
+TEST_F(DerivedFromBoard, TestAvailableMovesForBlackPawnOnSecondStep1) {
     handle_cell_click(&board[6][2]);
     handle_cell_click(&board[5][2]);
     handle_cell_click(&board[5][2]);
@@ -43,8 +43,43 @@ TEST_F(DerivedFromBoard, TestAvailableMovesForBlackPawnOnSecondStep) {
     EXPECT_EQ(distance(moves.begin(), moves.end()), 1);
 }
 
+
+TEST_F(DerivedFromBoard, TestAvailableMovesForWhitePawnOnSecondStep1) {
+    handle_cell_click(&board[1][1]);
+    handle_cell_click(&board[2][1]);
+    handle_cell_click(&board[2][1]);
+    auto moves = board[2][1].get_available_moves(board);
+    EXPECT_NE(std::find(moves.begin(), moves.end(), &board[3][1]), moves.end());
+    EXPECT_EQ(distance(moves.begin(), moves.end()), 1);
+}
+
+TEST_F(DerivedFromBoard, TestAvailableMovesForBlackPawnOnSecondStep2) {
+    handle_cell_click(&board[6][2]);
+    handle_cell_click(&board[4][2]);
+    handle_cell_click(&board[4][2]);
+    auto moves = board[4][2].get_available_moves(board);
+    EXPECT_NE(std::find(moves.begin(), moves.end(), &board[3][2]), moves.end());
+    EXPECT_EQ(distance(moves.begin(), moves.end()), 1);
+}
+
+
+TEST_F(DerivedFromBoard, TestAvailableMovesForWhitePawnOnSecondStep2) {
+    handle_cell_click(&board[1][1]);
+    handle_cell_click(&board[3][1]);
+    handle_cell_click(&board[3][1]);
+    auto moves = board[3][1].get_available_moves(board);
+    EXPECT_NE(std::find(moves.begin(), moves.end(), &board[4][1]), moves.end());
+    EXPECT_EQ(distance(moves.begin(), moves.end()), 1);
+}
+
 TEST_F(DerivedFromBoard, TestAvailableMovesForBlackPawnThatHasNoMove) {
     board[2][3].add_figure(new Pawn(Black));
     auto moves = board[2][3].get_available_moves(board);
+    EXPECT_EQ(distance(moves.begin(), moves.end()), 0);
+}
+
+TEST_F(DerivedFromBoard, TestAvailableMovesForWhitePawnThatHasNoMove) {
+    board[5][3].add_figure(new Pawn(White));
+    auto moves = board[5][3].get_available_moves(board);
     EXPECT_EQ(distance(moves.begin(), moves.end()), 0);
 }
